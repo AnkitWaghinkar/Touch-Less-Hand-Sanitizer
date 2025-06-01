a98a91e9f2b1e1dcd2cbc76fe260e5299cb678f7
AnkitWaghinkar <138594474+AnkitWaghinkar@users.noreply.github.com>
2025-06-01 15:06:33 +0530

    Update sanitizer.ino

--- sanitizer.ino
+++ sanitizer.ino
@@ -6,3 +6,18 @@ void setup()
   pinMode(RELAY, OUTPUT);
   pinMode(SENSOR, INPUT);
 }
+void loop()
+{
+  SENSOR_STATUS = digitalRead(SENSOR);
+  if (SENSOR_STATUS == LOW)                       
+{
+    digitalWrite(RELAY, HIGH);
+    delay(250);
+    digitalWrite(RELAY, LOW);
+    delay(2000);
+}
+  else 
+{
+    digitalWrite(RELAY, LOW);
+}
+}
